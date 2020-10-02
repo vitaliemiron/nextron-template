4e235908dc4ef54d0aaba79430baa13796eaa51c
Vitalie Miron <vm@insomniacdesign.com>
2020-10-02 17:01:30 +0300

    first commit

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -1,30 +1,30 @@
-import { InMemoryCache, NormalizedCacheObject } from "apollo-cache-inmemory";
-import { ApolloClient } from "apollo-client";
-import { HttpLink } from "apollo-link-http";
-import electron from "electron";
-import fetch from "isomorphic-fetch";
-import App, { Container } from "next/app";
-import React from "react";
+import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import electron from 'electron';
+import fetch from 'isomorphic-fetch';
+import App, { Container } from 'next/app';
+import React from 'react';
 
 let pyPort = 5000; // Actual port will be provided by main process
 let appGlobalClient = null as ApolloClient<NormalizedCacheObject>;
 
 const getAppGlobalClient = () => {
   return appGlobalClient;
-}
+};
 
 if (electron && electron.ipcRenderer) {
-  electron.ipcRenderer.on("pythonPort", ({}, arg) => {
+  electron.ipcRenderer.on('pythonPort', ({}, arg) => {
     pyPort = arg;
     appGlobalClient = new ApolloClient({
       cache: new InMemoryCache(),
       link: new HttpLink({
-        fetch:(fetch as any),
-        uri: "http://127.0.0.1:" + pyPort + "/graphql/",
+        fetch: fetch as any,
+        uri: 'http://127.0.0.1:5000/graphql/',
       }),
     });
   });
-  electron.ipcRenderer.send("getPythonPort");
+  electron.ipcRenderer.send('getPythonPort');
 }
 
 class MyApp extends App {
